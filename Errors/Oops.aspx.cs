dfc37fed75e19c9364ca72566dbedf1e047c709d
coachshon <shon.hogan@gmail.com>
2019-12-30 10:29:41 -0500

    Going node

--- Errors/Oops.aspx.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public partial class ErrorPages_Oops : BasePage
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-}
--- SenchaBuild.bat
+++
@@ -1,4 +0,0 @@
-D:
-cd D:\inetpub\wwwroot\ONCO_DEV\ClientLayer
-C:\Users\HOGAND11\bin\Sencha\Cmd\sencha app build
-pause
