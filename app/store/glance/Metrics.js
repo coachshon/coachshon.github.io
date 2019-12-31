92120058facc3704404b87ac3f798d156e5cee5a
coachshon <shon.hogan@gmail.com>
2019-12-31 14:11:45 -0500

    L8

--- app/store/glance/Metrics.js
+++ app/store/glance/Metrics.js
@@ -2,21 +2,15 @@
  * Summary metrics
  */
 Ext.define('App.store.glance.Metrics', {
-
-    extend: 'App.store.Base',
-    alias: 'store.glance-metrics',
-    /**
-    * @cfg {String} readPath: proxy end point, the webservice which will perform the given read operation.
-    */
-    readPath: 'Default.aspx/ReadGlanceMetrics',
-    /**
-    * Creates the store.
-    * @param {Object} config (optional) Config object
-    */
-    constructor: function (config) {
-        config = config || {};
-        this.callParent([config]);
-    }
+  extend: 'Ext.data.Store', 
+  alias: 'store.glance-metrics',
+  model: 'App.model.hco.Account',
+  proxy: {
+      type: 'ajax',
+      url: 'app/data/glance/Metrics.json',
+      reader:{
+          type: 'json',
+          root: 'd'
+      }
+  }   
 });
-
-
